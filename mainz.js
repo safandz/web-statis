356524e514dbc0418036443ad2133edab632e056
safandz <144671192+safandz@users.noreply.github.com>
2023-11-19 18:05:47 +0700

    Add files via upload

---
+++ mainz.js
@@ -0,0 +1,18 @@
+alert('Selamat Datang')
+
+const menu = document.querySelector ('.header #menu')
+const navbar = document.querySelector ('.navbar')
+
+menu.addEventListener('click', function())
+
+let menu = document.querySelector('#menu-icon')
+let navbar = document.querySelector('.navbar')
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
